4046827f0690122b26892d4b59f7ea6f74d817e3
rebecca fitzsimmons <rebecca@madetech.com>
2019-01-10 11:08:24 +0000

    Create CPI Fixed Data model

---
+++ db/migrate/20190110103926_create_cpi_fixed_data.rb
@@ -0,0 +1,14 @@
+class CreateCpiFixedData < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cpi_fixed_data do |t|
+      t.string :year
+      t.decimal :cpi
+      t.decimal :real
+      t.decimal :fr_uplift
+      t.decimal :rc_factor
+      t.decimal :rc_uplift
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/cpi_fixed_datum_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe CpiFixedDatum, type: :model do
+  it { should validate_presence_of(:year) }
+  it { should validate_presence_of(:rc_uplift) }
+end
