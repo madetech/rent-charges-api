e097d85db14e0ed049e125823f314d3bd18d236c
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2019-01-23 16:43:31 +0000

    Refactor fixed data domain object

--- lib/domain/fixed_data.rb
+++ lib/domain/fixed_data.rb
@@ -1,12 +1,7 @@
 module Domain
   class FixedData
+    include ActiveModel::Model
     attr_accessor :year,
                   :rc_uplift
-  
-     def initialize(**fixed_data)
-      fixed_data.each do |key, value|
-        self.send("#{key}=", value)
-     end
-    end
   end
 end
